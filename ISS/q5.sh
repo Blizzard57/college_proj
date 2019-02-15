5ebe2bf970287c5cba42a236e1a7faa892797450
Blizzard57 <kalp77000@gmail.com>
2019-02-16 00:25:20 +0530

    .

--- ISS/q5.sh
+++ ISS/q5.sh
@@ -3,7 +3,6 @@ find > var.txt
 find -printf %Ac > var2.txt
 cat var2.txt | sed 's/IST/IST\n/g' > asw.txt
 paste asw.txt var.txt > temp4.txt
-#This is a comment
 apropos lo | grep "^lo" >> 2018113003.txt
 awk 'length > max_length { max_length = length; longest_line = $0 } END { print longest_line }' ./2018113003.txt
 cat 2018113003.txt | wc -l
