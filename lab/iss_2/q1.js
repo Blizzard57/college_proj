29be19974b616a29d6cf761f4f1afe7a2bd48c82
Blizzard57 <kalp77000@gmail.com>
2019-03-08 14:13:13 +0530

    Small Changes

--- lab/iss_2/q1.js
+++
@@ -1,7 +0,0 @@
-function fifthDay(){
-	var today= new Date();
-	var weekday = [ "Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday" ];
-		var n = weekday[(today.getDay()+5)%7];
-console.log(n);
-}
-fifthDay();
