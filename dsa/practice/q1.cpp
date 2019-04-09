a649006e2ef7652a8215c3fff2b360c04703a763
Blizzard57 <kalp77000@gmail.com>
2019-04-09 16:30:03 +0530

    Small Changes

--- dsa/practice/q1.cpp
+++ dsa/practice/q1.cpp
@@ -1,17 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void dfs(int s,vector<vector<int>> &adj,int n){
-    vector<int> visit[n];
-    //visit[s]='g';
-    for(int i=0;i<adj[s].size;i++){
-        if(visit[adj[s][i]]!='g' && visit[adj[s][i]]!='b')
-            dfs(adj[s][i],adj,n);
-        else if(visit[adj[s][i]]=='b')
-            cout << "Loop Exists";
-    }
-    visit[s]='b';
+/*void dfs(int s,vector<vector<int>> &adj,int n){
+  vector<int> visit[n];
+//visit[s]='g';
+for(int i=0;i<adj[s].size;i++){
+if(visit[adj[s][i]]!='g' && visit[adj[s][i]]!='b')
+dfs(adj[s][i],adj,n);
+else if(visit[adj[s][i]]=='b')
+cout << "Loop Exists";
 }
+visit[s]='b';
+}*/
 
 int main(){
     int n,e,p,q;
@@ -23,21 +23,25 @@ int main(){
     }
     queue <int> bfs;
     vector <bool> visit(n,false);
-    /*int sh;
-    cout << "Enter Source  :";
-    cin >> sh;
-    bfs.push(sh);
-
-    while(!bfs.empty()){
-        int node = bfs.front();
-        bfs.pop();
-        visit[node]=true;
-        for(int i=0;i<adj[node].size();i++){
-            if(!visit[adj[node][i]])
-                bfs.push(adj[node][i]);
-                visit[adj[node][i]]=true;
+    int sh;
+    //cout << "Enter Source  :";
+    //cin >> sh;
+    for(int i=0;i<n;i++){
+        if(visit[i]==false){
+            sh = i;
+            bfs.push(sh);
+            while(!bfs.empty()){
+                int node = bfs.front();
+                bfs.pop();
+                visit[node]=true;
+                for(int i=0;i<adj[node].size();i++){
+                    if(!visit[adj[node][i]])
+                        bfs.push(adj[node][i]);
+                    visit[adj[node][i]]=true;
+                }
+                cout << node << " ";
+            }
         }
-        cout << node << " ";
-    }*/
+    }
     return 0;
 }
